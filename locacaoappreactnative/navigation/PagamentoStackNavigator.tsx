40093d7e10256dd7f72cca07b03d70c624b1190f
MPasin7 <matheus.pasin77@outlook.com>
2025-06-23 19:14:18 -0300

    CreatePagamento criada

--- locacaoappreactnative/navigation/PagamentoStackNavigator.tsx
+++ locacaoappreactnative/navigation/PagamentoStackNavigator.tsx
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import PagamentoScreen from '../screens/Pagamento/PagamentoScreen';
+import CreatePagamentoScreen from '@/screens/Pagamento/CreatePagamentoScreen';
 
 
 const Stack = createStackNavigator();
@@ -11,6 +12,7 @@ const PagamentoStackNavigator = () => {
         headerShown: false, 
     }}>
       <Stack.Screen name="PagamentoList" component={PagamentoScreen} />
+      <Stack.Screen name="CreatePagamento" component={CreatePagamentoScreen} />
       
     </Stack.Navigator>
   );
