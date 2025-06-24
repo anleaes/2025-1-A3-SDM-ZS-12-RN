53c691c5418db56a02149205c58cc767c6c4637c
MPasin7 <matheus.pasin77@outlook.com>
2025-06-24 15:27:04 -0300

    Resoluçoes de erros

--- locacaoappreactnative/screens/CarrinhoLocacao/CarrinhoScreen.tsx
+++ locacaoappreactnative/screens/CarrinhoLocacao/CarrinhoScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { ActivityIndicator, Alert, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import api from '../../services/api';
